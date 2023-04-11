8eeac986cd6c62a51dc0d0d889b53b941fba9896
Amine derbal <aminedoran31@gmail.com>
2023-04-11 14:01:33 +0100

    add variable that update the fetch

--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -5,17 +5,20 @@ import Loader from './Loader';
 import Rocket from './Rocket';
 
 const Rockets = () => {
-  const { rockets, isLoading, hasError } = useSelector((store) => store.rockets);
+  const {
+    rockets, isLoading, hasError, isFetched,
+  } = useSelector((store) => store.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getRocketsData());
-  }, [dispatch]);
+  }, [dispatch, isFetched]);
   if (isLoading) {
     return <Loader />;
   }
 
   if (hasError) return <h2>An error has occured</h2>;
+  if (rockets.length === 0) return <h2>No Rockets exist</h2>;
 
   return rockets.map((rocket) => <Rocket key={rocket.id} rocket={rocket} />);
 };
