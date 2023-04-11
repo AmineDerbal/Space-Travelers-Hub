3f491d1849ecd51e09d68251503f603889c65134
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-04-11 12:27:02 +0300

    fetch missions data and console log the required

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/Missions';
 
-const Missions = () => (
-  <div>Missions</div>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+  return (
+
+    <div>Missions</div>
+  );
+};
 
 export default Missions;
