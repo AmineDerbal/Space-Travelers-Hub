7907b4c5dd822434ac731824eb5b2be97c4f0fac
Amine derbal <aminedoran31@gmail.com>
2023-04-11 11:21:10 +0100

    add function to display rockets

--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getRocketsData } from '../redux/rockets/rocketsSlice';
+import Loader from './Loader';
+import Rocket from './Rocket';
 
 const Rockets = () => {
-  // const { rockets, isLoading, hasError } = useSelector((store) => store.rockets);
+  const { rockets, isLoading, hasError } = useSelector((store) => store.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getRocketsData());
   }, [dispatch]);
-  return <div>Rockets</div>;
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (hasError) return <h2>An error has occured</h2>;
+
+  return rockets.map((rocket) => <Rocket key={rocket.id} rocket={rocket} />);
 };
 
 export default Rockets;
