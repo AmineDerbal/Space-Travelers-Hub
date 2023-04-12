ff38cfe76f31598420cac6f4b9dd0317af79ddf9
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-04-12 15:39:24 +0300

    Add testing for Missions section

--- src/tests/Mission.test.js
+++ src/tests/Mission.test.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {
   render,
-  screen, fireEvent, within,
+  screen, fireEvent,
 } from '@testing-library/react';
 import configureMockStore from 'redux-mock-store';
 import store from '../Redux/store';
@@ -26,20 +26,20 @@ it('renders Missions correctly', () => {
 test('join a mission', () => {
   const mockStore = configureMockStore();
   const initialState = {
-        missions: {
-            missions: [
-              {
-                id: '1',
-                name: 'Thaicom',
-                description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.'
-              },
-            ],
-            isLoading: false
-            },
+    missions: {
+      missions: [
+        {
+          id: '1',
+          name: 'Thaicom',
+          description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+        },
+      ],
+      isLoading: false,
+    },
 
   };
-  let store;
-  store = mockStore(initialState);
+
+  const store = mockStore(initialState);
   store.clearActions();
   render(
     <Provider store={store}>
@@ -50,5 +50,5 @@ test('join a mission', () => {
   const joinButton = screen.getByRole('button', { name: /Join Mission/i });
   fireEvent.click(joinButton);
 
-  expect(store.getActions()).toEqual([{ type: 'missions/joinMission'}]);
+  expect(store.getActions()).toEqual([{ type: 'missions/joinMission' }]);
 });
